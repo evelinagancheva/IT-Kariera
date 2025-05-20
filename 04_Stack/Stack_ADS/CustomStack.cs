595a08ea993a3d45bbe1b18470c1e3b083f88fbc
Evelina Gancheva <35336732+evelinagancheva@users.noreply.github.com>
2025-05-20 08:52:11 +0300

    Add files via upload

---
+++ 04_Stack/Stack_ADS/CustomStack.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_ADS
+{
+    class CustomStack<T>
+    {
+        public const int INITIAL_CAPACITY = 16;
+
+        private T[] items;
+        public int Count { get; set; }
+
+        public CustomStack(int initialCapacity = INITIAL_CAPACITY)
+        {
+            this.items = new T[initialCapacity];
+        }
+
+        public void Push(T element)
+        {
+            if (this.Count == this.items.Length)
+            {
+                this.Grow();
+            }
+
+            this.items[this.Count] = element;
+            this.Count++;
+        }        
+
+        public T Pop()
+        {
+            if (this.Count == 0)
+            {
+                throw new InvalidOperationException("Empty stack");
+            }
+            this.Count--;
+            T element = this.items[this.Count];
+
+            T[] temp = new T[this.items.Length];
+            for (int i = 0; i < this.Count; i++)
+            {
+                temp[i] = this.items[i];
+            }
+
+            this.items = temp;
+            return element;
+        }
+
+        public T[] ToArray()
+        {
+            T[] temp = new T[this.Count];
+            for (int i = 0; i < this.Count; i++)
+            {
+                temp[i] = this.items[i];
+            }
+
+            return temp;
+        }
+
+        private void Grow()
+        {
+            T[] temp = new T[this.items.Length * 2];
+            for (int i = 0; i < this.Count; i++)
+            {
+                temp[i] = items[i];
+            }
+
+            this.items = temp;
+        }
+    }
+}
---
+++ 04_Stack/Stack_ADS/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stack_ADS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomStack<string> stack = new CustomStack<string>();
+            stack.Push("pesho");
+            stack.Push("gosho");
+            stack.Push("tosho");
+
+            Stack<int> stack1 = new Stack<int>();
+            stack1.Push(2);
+            stack1.Push(3);
+            stack1.Push(5);
+            Console.WriteLine(stack.Pop());
+            Console.WriteLine(stack.Count);
+        }
+    }
+}
+ 
