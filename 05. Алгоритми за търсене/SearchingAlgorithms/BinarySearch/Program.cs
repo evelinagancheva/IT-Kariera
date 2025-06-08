fa0fa08ff2793e249a2b0ec945ed188a25d28bd7
Evelina Gancheva <35336732+evelinagancheva@users.noreply.github.com>
2025-06-08 11:45:56 +0300

    Add files via upload

---
+++ 05. Алгоритми за търсене/SearchingAlgorithms/BinarySearch/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace BinarySearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //двоично търсене
+            int[] array = new int[] { 1, 2, 3, 8, 12, 19, 22, 33, 45, 84 };
+            int binary = BinarySearch(array, 8, 0, array.Length);
+            Console.WriteLine(binary);
+        }
+
+        public static int BinarySearch(int[] arr, int searchedNumber, int start, int end)
+        {
+            while (end >= start)
+            {
+                int mid = (start + end) / 2;
+                if (arr[mid] < searchedNumber)
+                    start = mid + 1;
+                else if (arr[mid] > searchedNumber)
+                    end = mid - 1;
+                else
+                    return mid;
+            }
+            return -1;
+        }
+    }
+}
---
+++ 05. Алгоритми за търсене/SearchingAlgorithms/LinearSearch/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Channels;
+
+namespace LinearSearch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //линейно търсене
+            List<int> list = new List<int>() { 5, 12, 1, 120, 84, 45, 57, 15, 18 };
+            int linear = LinearSearch(list, 120);
+            Console.WriteLine(linear);
+        }
+
+        public static int LinearSearch(List<int> nums, int searchedNumber)
+        {
+            for (int index = 0; index < nums.Count; index++)
+            {
+                if(searchedNumber== nums[index])
+                {
+                    return index;
+                }
+            }
+            return -1;
+        }
+    }
+}
